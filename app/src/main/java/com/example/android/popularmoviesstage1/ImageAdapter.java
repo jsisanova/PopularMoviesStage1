cbbe8e0c21847ede3f3352731c115f844a9b34d8
jsisanova <jana.sisanova@gmail.com>
2019-09-26 01:45:33 +0200

    Populate the RecycleView as a grid-like layout for now with dummy String data

---
+++ app/src/main/java/com/example/android/popularmoviesstage1/ImageAdapter.java
@@ -0,0 +1,80 @@
+package com.example.android.popularmoviesstage1;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
+
+    private String[] mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    // data is passed into the constructor
+    ImageAdapter(Context context, String[] data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    // inflates the cell layout from xml when needed
+    @Override
+    @NonNull
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.single_item_image, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each cell
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.myTextView.setText(mData[position]);
+//        holder.myImageView.setImageResource(Integer.parseInt(mData[position]));
+    }
+
+    // total number of items
+    @Override
+    public int getItemCount() {
+        return mData.length;
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView myTextView;
+//        ImageView myImageView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.movie_text);
+//            myImageView = itemView.findViewById(R.id.image);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) {
+                mClickListener.onItemClick(view, getAdapterPosition());
+            }
+        }
+    }
+
+    // convenience method for getting data at click position
+    String getItem(int id) {
+        return mData[id];
+    }
+
+    // allows clicks events to be caught
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+}
--- app/src/main/java/com/example/android/popularmoviesstage1/MainActivity.java
+++ app/src/main/java/com/example/android/popularmoviesstage1/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.android.popularmoviesstage1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ImageAdapter.ItemClickListener {
+
+    ImageAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // data to populate the RecyclerView with
+        String[] data = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"};
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
+        adapter = new ImageAdapter(this, data);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+
+        int numberOfColumns = 2;
+        recyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        String toastMessage = "You clicked number " + adapter.getItem(position) + ", which is at cell position " + position;
+        Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show();
+        Log.i("TAG", "You clicked number " + adapter.getItem(position) + ", which is at cell position " + position);
     }
 }
