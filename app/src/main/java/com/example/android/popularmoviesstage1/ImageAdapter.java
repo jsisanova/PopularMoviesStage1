67213263a44ba2a545415f6421f0957b15cf06ca
jsisanova <jana.sisanova@gmail.com>
2019-09-26 03:18:02 +0200

    Change dummy text for dummy images in grid-like layout in RecyclerView

--- app/src/main/java/com/example/android/popularmoviesstage1/ImageAdapter.java
+++ app/src/main/java/com/example/android/popularmoviesstage1/ImageAdapter.java
@@ -9,49 +9,35 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
 public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
 
-    private String[] mData;
+//    private String[] mData;
+    private List<String> mImageList;
+
     private LayoutInflater mInflater;
     private ItemClickListener mClickListener;
+    private final Context mContext;
 
     // data is passed into the constructor
-    ImageAdapter(Context context, String[] data) {
+    ImageAdapter(Context context, List imageList) {
         this.mInflater = LayoutInflater.from(context);
-        this.mData = data;
+        this.mContext = context;
+        this.mImageList = imageList;
     }
-
-    // inflates the cell layout from xml when needed
-    @Override
-    @NonNull
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = mInflater.inflate(R.layout.single_item_image, parent, false);
-        return new ViewHolder(view);
-    }
-
-    // binds the data to the TextView in each cell
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.myTextView.setText(mData[position]);
-//        holder.myImageView.setImageResource(Integer.parseInt(mData[position]));
-    }
-
-    // total number of items
-    @Override
-    public int getItemCount() {
-        return mData.length;
-    }
-
 
     // stores and recycles views as they are scrolled off screen
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        TextView myTextView;
-//        ImageView myImageView;
+//        TextView myTextView;
+        ImageView myImageView;
 
         ViewHolder(View itemView) {
             super(itemView);
-            myTextView = itemView.findViewById(R.id.movie_text);
-//            myImageView = itemView.findViewById(R.id.image);
+//            myTextView = itemView.findViewById(R.id.movie_text);
+            myImageView = itemView.findViewById(R.id.image);
             itemView.setOnClickListener(this);
         }
 
@@ -63,9 +49,47 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> 
         }
     }
 
+    // inflates the cell layout from xml when needed
+    @Override
+    @NonNull
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.single_item_image, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the ImageView in each item
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+//        holder.myTextView.setText(mData[position]);
+        final String path = mImageList.get(position);
+
+        Picasso.get()
+                .load(path)
+//                .centerCrop()
+//                .resize(250,250)
+                .fit()
+                .error(R.mipmap.ic_launcher_round)
+                .placeholder(R.mipmap.ic_launcher_round)
+                .into(holder.myImageView);
+
+        holder.myImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //handle click event on image
+            }
+        });
+    }
+
+    // total number of items
+    @Override
+    public int getItemCount() {
+//        return mData.length;
+        return mImageList.size();
+    }
+
     // convenience method for getting data at click position
     String getItem(int id) {
-        return mData[id];
+        return mImageList.get(id);
     }
 
     // allows clicks events to be caught
--- app/src/main/java/com/example/android/popularmoviesstage1/MainActivity.java
+++ app/src/main/java/com/example/android/popularmoviesstage1/MainActivity.java
@@ -4,9 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements ImageAdapter.ItemClickListener {
 
@@ -17,24 +21,43 @@ public class MainActivity extends AppCompatActivity implements ImageAdapter.Item
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // data to populate the RecyclerView with
-        String[] data = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"};
-
         // set up the RecyclerView
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
 
-        adapter = new ImageAdapter(this, data);
-        adapter.setClickListener(this);
-        recyclerView.setAdapter(adapter);
-
         int numberOfColumns = 2;
         recyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
+
+        // data to populate the RecyclerView with
+        String[] data = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"};
+
+        List<String> imageList = new ArrayList<>();
+        imageList.add("https://farm5.staticflickr.com/4403/36538794702_83fd8b63b7_c.jpg");
+        imageList.add("https://farm5.staticflickr.com/4354/35684440714_434610d1d6_c.jpg");
+        imageList.add("https://farm5.staticflickr.com/4301/35690634410_f5d0e312cb_c.jpg");
+        imageList.add("https://farm4.staticflickr.com/3854/32890526884_7dc068fedd_c.jpg");
+        imageList.add("https://farm8.staticflickr.com/7787/18143831616_a239c78056_c.jpg");
+        imageList.add("https://farm9.staticflickr.com/8745/16657401480_57653ac8b0_c.jpg");
+        imageList.add("https://farm3.staticflickr.com/2917/14144166232_44613c53c7_c.jpg");
+        imageList.add("https://farm8.staticflickr.com/7453/13960410788_3dd02b7a02_c.jpg");
+        imageList.add("https://farm1.staticflickr.com/920/29297133218_de38a7e4c8_c.jpg");
+        imageList.add("https://farm2.staticflickr.com/1788/42989123072_6720c9608d_c.jpg");
+        imageList.add("https://farm1.staticflickr.com/888/29062858008_89851766c9_c.jpg");
+        imageList.add("https://farm2.staticflickr.com/1731/27940806257_8067196b41_c.jpg");
+        imageList.add("https://farm1.staticflickr.com/884/42745897912_ff65398e38_c.jpg");
+        imageList.add("https://farm2.staticflickr.com/1829/27971893037_1858467f9a_c.jpg");
+        imageList.add("https://farm2.staticflickr.com/1822/41996470025_414452d7a0_c.jpg");
+        imageList.add("https://farm2.staticflickr.com/1793/42937679651_3094ebb2b9_c.jpg");
+        imageList.add("https://farm1.staticflickr.com/892/42078661914_b940d96992_c.jpg");
+
+        adapter = new ImageAdapter(this, imageList);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
     public void onItemClick(View view, int position) {
-        String toastMessage = "You clicked number " + adapter.getItem(position) + ", which is at cell position " + position;
+        String toastMessage = "You clicked on item at cell position " + position;
         Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show();
-        Log.i("TAG", "You clicked number " + adapter.getItem(position) + ", which is at cell position " + position);
+        Log.i("TAG", "You clicked on item at cell position " + position);
     }
 }
